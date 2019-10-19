2e7c3fe078b07c6edb95cf2f269fc88ec0f98301
fhorstty <oasis.in.autumn.ruby@gmail.com>
2019-10-19 21:12:13 +0900

    [add] error handler

--- my-express/app.js
+++ my-express/app.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
+const createError = require('http-errors');
 const debug = require('debug')('app');
-const morgran = require('morgran');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
 const config = require('./config');
 
 const app = express();
+
+// logging
+app.use(morgan('short'));
 
 // static files
 app.use(express.static('public'));
@@ -14,7 +18,7 @@ app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// html body parser
+// middleware: html body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -22,6 +26,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const indexRouter = require('./routes/index');
 
 app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.error = req.app.get('env') === 'development'
+    ? err
+    : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 const port = config.app.port || 3000;
 app.listen(port);
